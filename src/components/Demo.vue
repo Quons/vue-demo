502e1b388756f61eb6445d6914b1f2099e3e6233
quon <liuyc@knowbox.cn>
2019-03-27 21:34:16 +0800

    first commit

---
+++ src/components/Demo.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1 :class="classObj">this is a h1 tag</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "vueDemo",
+  data() {
+    return {
+      classObj: { 'red': true, active: true, italic: true }
+    };
+  },
+  methods: {
+    calculate() {}
+  }
+};
+</script>
+<style>
+.red {
+  color: red;
+}
+.thin {
+  font-weight: 200;
+}
+.italic {
+  font-style: italic;
+}
+.active {
+  letter-spacing: 1pc;
+}
+</style>
