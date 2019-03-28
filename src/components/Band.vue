b0f489ae6d61f60a37bb6ad4b60d000558e3d641
quon <liuyc@knowbox.cn>
2019-03-28 12:43:03 +0800

    brand

---
+++ src/components/Band.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <link
+      rel="stylesheet"
+      href="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/css/bootstrap.min.css"
+    >
+    <table class="table table-bordered table-hover table-striped">
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Name</th>
+          <th>Ctime</th>
+          <th>Operation</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+ 
+<script>
+export default {
+  name: "vueDemo",
+  data() {
+    return {
+    list:[
+      {},
+    ]
+    };
+  },
+  methods: {
+    toggle() {
+      this.flag = !this.flag;
+    }
+  }
+};
+</script>
+<style>
+.red {
+  color: red;
+}
+.thin {
+  font-weight: 200;
+}
+.italic {
+  font-style: italic;
+}
+.active {
+  letter-spacing: 1pc;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,12 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import VueDemo from '@/components/Demo'
+import BandDemo from '@/components/Band'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
+  routes: [{
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
@@ -16,6 +16,11 @@ export default new Router({
       path: '/vue-demo',
       name: 'vue-demo',
       component: VueDemo
+    },
+    {
+      path: '/band-demo',
+      name: 'vue-demo',
+      component: BandDemo
     }
   ]
 })
